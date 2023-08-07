4838e863a5134f57c64802fe20e672594eb7f5f8
JA54312 <ija54312@gmail.com>
2023-08-07 17:34:04 -0600

    router y estilos base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,69 @@
+//Pages
 import Home from "./pages/home/Home"
+import Users from "./pages/users/Users"
+import Products from "./pages/products/Produtcts"
+import NavBar from "./components/navbar/NavBar"
+import Footer from "./components/footer/Footer"
+import Menu from "./components/menu/Menu"
+import Login from "./pages/login/Login"
+//Styles
+import './styles/global.scss'
+//Router
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom"
 
 function App() {
 
+  const Layout = () => {
     return (
-      <div><Home/></div>
+      <div className="main">
+        <NavBar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
     )
   }
-  
-  export default App
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Layout />
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        },
+        {
+          path: "users",
+          element: <Users />
+        },
+        {
+          path: "products",
+          element: <Products />
+        }
+      ]
+    },
+    {
+      path: "login",
+      element: <Login />
+    }
+  ])
+
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,9 @@
+
+
+const NavBar = () => {
+    return (
+        <div className="navbar">NavBar</div>
+    )
+}
+
+export default NavBar

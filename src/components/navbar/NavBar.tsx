512e487b741b6a1e7b250fdd1ed8a1ca232ed26b
JA54312 <ija54312@gmail.com>
2023-08-08 14:14:56 -0600

    navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,9 +1,32 @@
+//Styles
+import "./NavBar.scss";
 
-
-const NavBar = () => {
+const Navbar = () => {
     return (
-        <div className="navbar">NavBar</div>
-    )
-}
+        <div className="navbar">
+            <div className="logo">
+                <img src="logo.svg" alt="" />
+                <span>lamadmin</span>
+            </div>
+            <div className="icons">
+                <img src="/search.svg" alt="" className="icon" />
+                <img src="/app.svg" alt="" className="icon" />
+                <img src="/expand.svg" alt="" className="icon" />
+                <div className="notification">
+                    <img src="/notifications.svg" alt="" />
+                    <span>1</span>
+                </div>
+                <div className="user">
+                    <img
+                        src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+                        alt=""
+                    />
+                    <span>Jane</span>
+                </div>
+                <img src="/settings.svg" alt="" className="icon" />
+            </div>
+        </div>
+    );
+};
 
-export default NavBar
+export default Navbar;

e994eb3b50fbb08fb02241b7e59b0b7ad2d3dfde
JA54312 <ija54312@gmail.com>
2023-08-14 12:54:40 -0600

    chartbox

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className="navbar">
             <div className="logo">
                 <img src="logo.svg" alt="" />
-                <span>lamadmin</span>
+                <span>ja54312</span>
             </div>
             <div className="icons">
                 <img src="/search.svg" alt="" className="icon" />
